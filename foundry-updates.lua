896d267cc433d746e50a7bf007255ad1345320a6
Brevven <brevingame@gmail.com>
2022-01-01 02:43:29 -0800

    fix steel

--- foundry-updates.lua
+++ foundry-updates.lua
@@ -2,7 +2,7 @@ local util = require("data-util")
 
 util.set_to_founding("steel-plate")
 util.replace_some_ingredient("steel-plate", "iron-plate", 1, util.me.carbon(), 1)
-util.multiply_time("stee-plate", 4/5)
+util.multiply_time("steel-plate", 4/5)
 util.add_prerequisite("steel-processing", "foundry")
 
 util.set_to_founding("tungsten-carbide")
@@ -17,6 +17,7 @@ util.set_to_founding("crucible") -- from graphite
 
 -- K2
 for i, machine in pairs(util.me.get_other_machines()) do
-  log("BZAdding "..machine)
+  log("Allowing "..machine.." to handle founding")
   util.add_crafting_category("assembling-machine", machine, "founding")
 end
+

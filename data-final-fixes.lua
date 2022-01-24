8835ffd8759c4a35fd2d82efae4b3e115af1a94a
Brevven <brevingame@gmail.com>
2022-01-24 00:17:13 -0800

    coke stacking

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -4,3 +4,14 @@ local util = require("data-util")
 if mods.Bio_Industries then
   util.remove_ingredient("foundry", "silica")
 end
+
+
+
+if data.raw.item["coke"] and not mods.Krastorio2 then
+  if deadlock then
+    deadlock.add_stack("coke", nil, "deadlock-stacking-1")
+  end
+  if deadlock_crating then
+    deadlock_crating.add_crate("coke", "deadlock-crating-1")
+  end
+end

c1e2cbcb8cc402f3c7ee1b52e9dbf426a3842f3a
Brevven <brevingame@gmail.com>
2022-08-07 17:11:42 -0700

    stacked icon

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -10,7 +10,7 @@ end
 
 if data.raw.item["coke"] and not mods.Krastorio2 then
   if deadlock then
-    deadlock.add_stack("coke", nil, "deadlock-stacking-1")
+    deadlock.add_stack("coke", "__bzfoundry__/graphics/icons/coke-stacked.png", "deadlock-stacking-1", 128)
   end
   if deadlock_crating then
     deadlock_crating.add_crate("coke", "deadlock-crating-1")
